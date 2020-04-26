d615c418ff59bdde0f29c274cb2a1565ca14de3b
Felipe Marcon <felipeemarcon@gmail.com>
2020-04-26 13:50:08 -0300

    feat: create Expressions Boxes in DOM

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const main = document.querySelector("main");
+
 const humanExpressions = [
   { img: "./img/drink.jpg", text: "Estou com sede" },
   { img: "./img/food.jpg", text: "Estou com fome" },
@@ -12,3 +14,17 @@ const humanExpressions = [
   { img: "./img/school.jpg", text: "Quero ir para a escola" },
   { img: "./img/grandma.jpg", text: "Quero ver a vovó" },
 ];
+
+const createExpressionBox = ({ img, text }) => {
+  const div = document.createElement("div");
+
+  div.classList.add("expression-box");
+  div.innerHTML = `
+    <img src="${img}" alt="${text}"/>
+    <p class="info">${text}</p>
+  `;
+
+  main.appendChild(div);
+};
+
+humanExpressions.forEach(createExpressionBox);
